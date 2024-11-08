157cf590e65f2a6788230f71e668a5b59b7c5f44
Leonardo Tórtoro Pereira <leonardo.t.pereira13@gmail.com>
2024-11-07 23:45:28 -0300

    Refactors the code and removes some extras

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -1,145 +1,95 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerShot : MonoBehaviour
+public class PlayerShooter : MonoBehaviour
 {
-    private struct BulletForceAndRotation
-    {
-        public Vector2 Force;
-        public int Rotation;
-    }
-    [SerializeField]
-    protected float shootSpeed, atkSpeed;
-    private bool _canShoot;
-    private bool _isHoldingShoot;
-    [SerializeField] private GameObject bulletSpawn;
-    [SerializeField] private GameObject bulletPrefab;
-    private Rigidbody2D _rigidbody2D;
-    private const float _minShootMagnitude = 0.01f;
-    private const float _maxShootDir = 0.125f;
+	[field: SerializeField]
+	public float AttackSpeed { get; private set; }
+	[field: SerializeField]
+	private GameObject BulletSpawn { get; set; }
+	[field: SerializeField]
+	private GameObject BulletPrefab { get; set; }
+	private float ProjectileSpeed { get; set; }
+	private bool CanShoot { get; set; }
+	private bool IsHoldingShoot { get; set; }
+	private Rigidbody2D Body { get; set; }
+	private const float MinShootMagnitude = 0.01f;
 
-    private void Awake()
-    {
-        _canShoot = true;
-    }
+	private struct BulletForceAndRotation
+	{
+		public Vector2 Force;
+		public int Rotation;
+	}
+	
+	private void Awake()
+	{
+		CanShoot = true;
+		Body = GetComponent<Rigidbody2D>();
 
-    private void Start()
-    {
-        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-    }
+	}
+	
+	public void OnShoot(InputAction.CallbackContext context)
+	{
+		if(context.performed)
+		{
+			IsHoldingShoot = true;
+		}
+		else if(context.canceled)
+		{
+			IsHoldingShoot = false;
+		}
 
-    /*
-    private void OnEnable()
-    {
-        PlayerController.PlayerDeathEventHandler += StopInput;
-        PlayerController.SceneLoaded += StartInput;
-        SceneManager.sceneLoaded += OnLevelFinishedLoading;
-    }
+		var shootDirection = context.ReadValue<Vector2>();
+		shootDirection.Normalize();
+		if (shootDirection.magnitude > MinShootMagnitude)
+			StartCoroutine(ShootBullet(shootDirection));
+	}
 
-    private void OnDisable()
-    {
-        PlayerController.PlayerDeathEventHandler -= StopInput;
-        PlayerController.SceneLoaded -= StartInput;
-        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
-    }
-    */
+	private IEnumerator ShootBullet(Vector2 shootDirection)
+	{
+		while (IsHoldingShoot)
+		{
+			yield return null;
+			if (!CanShoot) continue;
+			var bulletForceAndRotation = GetBulletForceAndRotation(shootDirection);
+			RotateSpawnPoint(bulletForceAndRotation.Rotation);
+			SpawnAndShootBullet(bulletForceAndRotation);
+			var coolDownTime = GetCooldown();
+			StartCoroutine(CountCooldown(coolDownTime));
+		}
+	}
 
-    public void Shoot(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            _isHoldingShoot = true;
-            //PlayerAnimator.SetBool(IsShooting, true);
-        }
-        else if (context.canceled)
-        {
-            _isHoldingShoot = false;
-            //PlayerAnimator.SetBool(IsShooting, false);
-        }
-        var shotDirection = context.ReadValue<Vector2>();
-        shotDirection.Normalize();
-        if (shotDirection.magnitude > _minShootMagnitude)
-            StartCoroutine(ShootBullet(shotDirection));
-    }
+	private BulletForceAndRotation GetBulletForceAndRotation(Vector2 projectileDirection)
+	{
+		BulletForceAndRotation bulletForceAndRotation;
+		bulletForceAndRotation.Force = new Vector2(0, 1);
+		bulletForceAndRotation.Rotation = 0;
+		return bulletForceAndRotation;
+	}
 
-    private IEnumerator ShootBullet(Vector2 shotDirection)
-    {
-        while (_isHoldingShoot)
-        {
-            yield return null;
-            if (!_canShoot) continue;
-            var bulletForceAndRotation = GetBulletForceAndRotation(shotDirection);
-            //UpdateShotAnimation(shotDirection);
-            RotateSpawnPoint(bulletForceAndRotation);
-            SpawnAndShootBullet(bulletForceAndRotation);
-            var coolDownTime = GetCooldown();
-            StartCoroutine(CountCooldown(coolDownTime));
-        }
-    }
+	private void RotateSpawnPoint(int rotation)
+	{
+		BulletSpawn.transform.rotation = Quaternion.Euler(0, 0, rotation);
+	}
 
-    private float GetCooldown()
-    {
-        return 1.0f / atkSpeed;
-    }
+	private void SpawnAndShootBullet(BulletForceAndRotation bulletForceAndRotation)
+	{
+		var bullet = Instantiate(BulletPrefab, BulletSpawn.transform.position, BulletSpawn.transform.rotation);
+		var projectileController = bullet.GetComponent<ProjectileController>();
+		projectileController.Shoot(bulletForceAndRotation.Force + Body.velocity.normalized);
+	}
 
-    private void SpawnAndShootBullet(BulletForceAndRotation bulletForceAndRotation)
-    {
-        var bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-        var bulletController = bullet.GetComponent<ProjectileController>();
-        bulletController.Shoot(bulletForceAndRotation.Force + _rigidbody2D.velocity.normalized);
-    }
+	private float GetCooldown()
+	{
+		return 1.0f / AttackSpeed;
+	}
 
-    private void RotateSpawnPoint(BulletForceAndRotation bulletForceAndRotation)
-    {
-        bulletSpawn.transform.rotation = Quaternion.Euler(0, 0, bulletForceAndRotation.Rotation);
-    }
-
-    private BulletForceAndRotation GetBulletForceAndRotation(Vector2 shotDirection)
-    {
-        const int rotationOffset = -45;
-        BulletForceAndRotation bulletForceAndRotation;
-        if (shotDirection.x > _maxShootDir)
-        {
-            bulletForceAndRotation.Rotation = 0 + rotationOffset;
-            bulletForceAndRotation.Force = new Vector2(shootSpeed, 0f);
-        }
-        else if (shotDirection.x < -_maxShootDir)
-        {
-            bulletForceAndRotation.Rotation = 180 + rotationOffset;
-            bulletForceAndRotation.Force = new Vector2(-shootSpeed, 0f);
-        }
-        else if (shotDirection.y > _maxShootDir)
-        {
-            bulletForceAndRotation.Rotation = 90 + rotationOffset;
-            bulletForceAndRotation.Force = new Vector2(0f, shootSpeed);
-        }
-        else
-        {
-            bulletForceAndRotation.Rotation = 270 + rotationOffset;
-            bulletForceAndRotation.Force = new Vector2(0f, -shootSpeed);
-        }
-
-        return bulletForceAndRotation;
-    }
-
-    private IEnumerator CountCooldown(float bulletCooldown)
-    {
-        _canShoot = false;
-        yield return new WaitForSeconds(bulletCooldown);
-        _canShoot = true;
-    }
-
-    private void StartInput(object sender, EventArgs eventArgs)
-    {
-        _canShoot = true;
-    }
-
-    private void StopInput(object sender, EventArgs eventArgs)
-    {
-        _canShoot = false;
-    }
-
+	private IEnumerator CountCooldown(float bulletCooldown)
+	{
+		CanShoot = false;
+		yield return new WaitForSeconds(bulletCooldown);
+		CanShoot = true;
+	}
 }
--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -1,81 +1,25 @@
-using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
-{
-    public static event EventHandler EnemyHitEventHandler;
-    public static event EventHandler PlayerHitEventHandler;
-    private Vector2 pos, moveDir;
-    private float MoveSpeed { get; set; }
-    [SerializeField]
-    private int damage;
+{	
+	[field: SerializeField]
+	public float Speed { get; private set; }
+	[field: SerializeField]
+	public int Damage { get; private set; }
+	
+	private Vector2 Position { get; set; }
+	private Vector2 Movement { get; set; }
+	private Rigidbody2D Body { get; set; }
 
-    private Rigidbody2D bulletRigidBody;
+	private void Start()
+	{
+		Body = GetComponent<Rigidbody2D>();
+	}
 
-    private void Start()
-    {
-        bulletRigidBody = GetComponent<Rigidbody2D>();
-    }
-
-
-    public void DestroyBullet()
-    {
-        GetComponent<Collider2D>().enabled = false;
-        GetComponent<SpriteRenderer>().enabled = false;
-        Destroy(gameObject);
-    }
-
-    private void OnEnemyHit()
-    {
-        EnemyHitEventHandler?.Invoke(null, EventArgs.Empty);
-    }
-
-    private void OnPlayerHit()
-    {
-        PlayerHitEventHandler?.Invoke(null, EventArgs.Empty);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        var collisionDirection = Vector3.Normalize(collision.gameObject.transform.position - gameObject.transform.position);
-        if (CompareTag("EnemyBullet"))
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                OnPlayerHit();
-                //collision.gameObject.GetComponent<HealthController>().ApplyDamage(damage, collisionDirection);
-                DestroyBullet();
-            }
-        }
-        else if (CompareTag("Bullet"))
-        {
-            if (collision.gameObject.CompareTag("Enemy"))
-            {
-                OnEnemyHit();
-                //collision.gameObject.GetComponent<EnemyController>().ApplyDamageEffects(collisionDirection);
-                //collision.gameObject.GetComponent<HealthController>().ApplyDamage(damage, collisionDirection);
-                DestroyBullet();
-            }
-            if (collision.gameObject.CompareTag("Shield"))
-            {
-                DestroyBullet();
-            }
-        }
-        if (collision.gameObject.CompareTag("Block"))
-        {
-            DestroyBullet();
-        }
-    }
-
-    public void Shoot(Vector2 facingDirection)
-    {
-        StartCoroutine(ShootAfterStart(facingDirection));
-    }
-
-    public IEnumerator ShootAfterStart(Vector2 facingDirection)
-    {
-        yield return new WaitForEndOfFrame();
-        bulletRigidBody.AddForce(facingDirection, ForceMode2D.Impulse);
-    }
+	public void Shoot(Vector2 direction)
+	{
+		Body.AddForce(direction, ForceMode2D.Impulse);
+	}
 }
